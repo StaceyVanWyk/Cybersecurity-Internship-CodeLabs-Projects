f7f521d52fe915560143a90636095e89f05f3b6b
Stacey Van Wyk <svanwyk722@gmail.com>
2026-04-25 11:05:19 +0200

    Updated README and project completetion

--- Project1/password_checker.py
+++ Project1/password_checker.py
@@ -1,39 +1,35 @@
 
-#Step 1: This is the INPUT stage
+# STEP 1: INPUT
+# =========================
 password = input("Enter your password: ")
 
-#Step 2: Analysis - Create variable
-
+# STEP 2: ANALYSIS
+# =========================
 length = len(password)
 has_upper = any(char.isupper() for char in password)
 has_digit = any(char.isdigit() for char in password)
 has_symbol = any(not char.isalnum() for char in password)
 
-# #Step 3: Scoring logic
+
+# STEP 3: SCORING
+# =========================
 score = 0
 
-# if length >= 8:
-#     score += 1
-# if has_upper:
-#     score += 1
-# if has_digit:
-#     score += 1
-# if has_symbol:
-#     score += 1
+if has_upper:
+    score += 1
+if has_digit:
+    score += 1
+if has_symbol:
+    score += 1
 
-#Step 4: Output
+
+# STEP 4: OUTPUT
+# =========================
 if length < 8:
     print("Weak password")
+elif score == 1:
+    print("Medium password")
 else:
-    if score <=2:
-        print("Medium")
-    else:
-        print("Strong password")
-# if score <= 1:
-#     print("Weak password")
-# elif score <=3:
-#     print("Medium password")
-# else:
-#     print("Strong password")
+    print("Strong password")
     
 
